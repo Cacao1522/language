788d56d311d698b00805a79036366149523bb565
Cacao1522 <calstone841@gmail.com>
2024-04-25 13:00:09 +0900

    cse test commit

--- task3-1.c
+++ task3-1.c
@@ -1,13 +1,10 @@
-Struct HeavyData
-{
+struct HeavyData {
     double data[10000];
     double index[10000];
 };
-int main()
-{
+int main() {
     struct HeavyData data;
-    for (int i = 0; i < 10000; i++)
-    {
+    for (int i = 0; i < 10000; i++) {
         data.data[i] = i;
         data.index[i] = i;
     }
