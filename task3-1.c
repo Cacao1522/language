36e087b2e98840b6bd6bafd8546a80106dbe7d4b
Cacao1522 <calstone841@gmail.com>
2024-05-07 00:00:26 +0900

    a

--- task3-1.c
+++ task3-1.c
@@ -1,12 +1,30 @@
-struct HeavyData {
+#include <stdio.h>
+
+struct HeavyData
+{
     double data[10000];
     double index[10000];
 };
-int main() {
+void value(struct HeavyData data, int n)
+{
+    if (n > 0)
+        value(data, n - 1);
+    else
+        return;
+}
+void pointer(struct HeavyData *data, int n)
+{
+    if (n > 0)
+        pointer(data, n - 1);
+    else
+        return;
+}
+
+int main()
+{
     struct HeavyData data;
-    for (int i = 0; i < 10000; i++) {
-        data.data[i] = i;
-        data.index[i] = i;
-    }
+    value(data, 10);
+    // pointer(&data, 50);
+    char c = getchar();
     return 0;
 }
--- task3-2.c
+++ task3-2.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int n;
     int stack[20];
     int stackPoint = 0;
     scanf("%d", &n);
     int ret = 1;
+    goto CHE;
+CHE:
+    if (n < 0)
+        goto ERR;
+    if (n == 0)
+        goto END;
+    goto LOAD;
 LOAD:
-    if (n < 0) goto ERR;
-    if (n == 0) goto END;
     stack[stackPoint++] = n;
     n--;
-    if (n > 0) goto LOAD;
+    if (n > 0)
+        goto LOAD;
+    goto CAL;
 CAL:
     ret *= stack[--stackPoint];
-    if (stackPoint - 1 < 0) goto END;
+    if (stackPoint - 1 < 0)
+        goto END;
     goto CAL;
-
 END:
     printf("%d\n", ret);
     return 0;
