4c96deaa5ff6cf0319af079fc6d5c1257ebe4701
Cacao1522 <calstone841@gmail.com>
2024-07-14 01:32:39 +0900

    a

--- func/task10-1.hs
+++ func/task10-1.hs
@@ -1,6 +1,19 @@
-fact 0 = 1
-fact 1 = 1
-fact n = fact(n-1)+ fact(n-2)
+fib 0 = 0
+fib 1 = 1
+fib n = fib(n-1)+ fib(n-2)
+
+my_length [] = 0
+my_length (_:xs) = my_length xs + 1
+ 
+my_sum [] = 0
+my_sum (x:xs) = my_sum xs + x
+
+my_reverse [] = []
+my_reverse (x:xs) = my_reverse xs ++ [x]
 
 main = do
-    print $ fact 5
+    print $ fib 5
+    print $ my_length [1,2,3]
+    print $ my_length []
+    print $ my_sum [1..5]
+    print $ my_reverse [1..5]
